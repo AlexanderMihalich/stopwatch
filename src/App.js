328660909a3bc293479c3c13869e4b36059d7214
AlexanderMihalich <78353628+AlexanderMihalich@users.noreply.github.com>
2021-08-31 12:32:00 +0300

    Observables with Rxjs and the backbone of the stopwatch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { interval, Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+import Buttons from "./components/Buttons";
 
 const App = () => {
-	return (
-		<div>
 
-		</div>
+	const [time, setTime] = useState(0);
+	const [start, setStart] = useState(false)
+
+	useEffect(() => {
+		const stopWatch$ = new Subject();
+		interval(10)
+			.pipe(takeUntil(stopWatch$))
+			.subscribe(() => {
+				if (start === true) {
+					setTime(value => value + 10);
+				}
+			});
+		return () => {
+			stopWatch$.next();
+			stopWatch$.complete();
+		};
+	}, [start]);
+
+
+	return (
+		<center>
+			<h1>
+				<span>{("0" + Math.floor((time / 3600000) % 24)).slice(-2)} </span>
+				<span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}</span>
+				<span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span>
+			</h1>
+			<Buttons />
+		</center>
 	);
 }
 
---
+++ src/components/Buttons.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Buttons = () => {
+	return (
+		<div>
+			<button>Start</button>
+			<button>Stop</button>
+			<button>Reset</button>
+			<button>Wait</button>
+		</div>
+	)
+}
+
+export default Buttons
