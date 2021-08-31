248633d5b868112af1ec26317b009b33c6f76663
AlexanderMihalich <78353628+AlexanderMihalich@users.noreply.github.com>
2021-08-31 14:11:37 +0300

    double click is implemented using a timer

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,11 +1,30 @@
 import React from 'react'
 
 const Buttons = ({ timeStart, timeStop, timeReset, timeWait, startActive }) => {
+
+	// ================ detailed timer for the wait button =================
+	// let wasClicked = false;
+	// let timeout;
+	// const doubleClick = () => {
+	// 	if (wasClicked) {
+	// 		wasClicked = false;
+	// 		clearTimeout(timeout);
+	// 		return;
+	// 	}
+	// 	wasClicked = true;
+	// 	timeout = setTimeout(() => {
+	// 		timeWait()
+	// 		wasClicked = false;
+	// 	}, 300);
+	// }
+	// ================ detailed timer for the wait button =================
+
 	return (
 		<div className="stopWatch__buttons">
 			<button className="stopWatch__button" disabled={startActive} onClick={timeStart}>Start</button>
 			<button className="stopWatch__button" disabled={!startActive} onClick={timeStop}>Stop</button>
-			<button className="stopWatch__button" disabled={!startActive} onClick={timeWait}>Wait</button>
+			{/* <button className="stopWatch__button" disabled={!startActive} onClick={doubleClick}>Wait</button> */}
+			<button className="stopWatch__button" disabled={!startActive} onClick={() => setTimeout(timeWait, 300)}>Wait</button>
 			<button className="stopWatch__button" disabled={!startActive} onClick={timeReset}>Reset</button>
 		</div>
 	)
